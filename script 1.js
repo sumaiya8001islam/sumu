526bc679370972f953e3335ef2cafdfccacc1ad2
Sumaiya islam (sumu) <sadiasakib331@gmail.com>
2024-10-18 19:18:13 -1200

    Add files via upload

---
+++ script 1.js
@@ -0,0 +1,9 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Login functionality not implemented yet!');
+});
+
+document.getElementById('registrationForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Registration functionality not implemented yet!');
+});
